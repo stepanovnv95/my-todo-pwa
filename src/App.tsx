0f456d3564cef791178f10fba309c56f4663849f
Stepanov Nikita <stepanovnv95@gmail.com>
2021-03-20 22:35:41 +0400

    App changed to class, added TaskList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import TaskList from './TaskList'
 
-function App() {
-  return (
-    <div className="App"></div>
-  );
+class App extends React.PureComponent 
+{
+  render() {
+    return (
+      <div className="App">
+        <TaskList tasks={['Task 1', 'Task 2', 'Task 3']} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/TaskList.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface TaskListProps {
+  tasks: string[];
+}
+
+class TaskList extends React.PureComponent<TaskListProps> {
+  render() {
+    return (
+      <>
+        {this.body()}
+      </>
+    );
+  }
+  body(): JSX.Element {
+    if (this.props.tasks.length > 0) {
+      return <ul>{this.list()}</ul>;
+    } else {
+      return <p>Empty</p>;
+    }
+  }
+  list(): JSX.Element[] {
+    const l = this.props.tasks.map((t): JSX.Element => {
+      return <li>{t}</li>;
+    });
+    return l;
+  }
+}
+
+export default TaskList;
